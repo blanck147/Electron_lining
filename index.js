2e5c1c466d43645401e099d176bfa38d5a18b2bb
blanck147 <938230600@qq.com>
2019-03-29 17:53:43 +0800

    Revert "Revert "Revert "test2"""
    This reverts commit af6ec68398d69b02ea53b8e47f026f0e8e0cc736.

---
+++ index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron'); // 在electron 中有 app 这个对象 和BrowserWindow 这个类
+
+function createWindow() {
+    win = new BrowserWindow({ width: 800, height: 600 }); //在这里是创建一个BrowserWindow的实例，就是一个窗口，设定大小
+    win.loadFile('index.html'); //加载了主窗口UI的页面文件
+    win.on('closed',()=>{
+        console.log('closed');
+        win = null
+    })
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed',()=>{
+    console.log('window-all-closed');
+    if(process.platform != 'darwin'){
+        app.quit();
+    }
+});
+
+app.on('active',()=>{
+    console.log('active');
+    if (win == null) {
+        createWindow();//在苹果系统下面是会再次获得焦点，并没有真正的关闭，只适用苹果系统
+    }
+});
